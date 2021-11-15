4ab3dc77a073d2d0a52f8ac8cbb69a6d5330ee94
Wang <zhiwen.wang@epitech.eu>
2021-11-15 19:07:53 +0100

    [ADD] more function validators

--- lib/validators.dart
+++ lib/validators.dart
@@ -1,24 +1,59 @@
+RegExp _email = RegExp(
+    r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$");
+
+RegExp _alphanumeric = RegExp(r'^[a-zA-Z0-9]+$');
+RegExp _alpha = RegExp(r'^[a-zA-Z]+$');
+RegExp _numeric = RegExp(r'^-?[0-9]+$');
+
+RegExp _passwordStrong =
+    RegExp(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$');
+
 class Validators {
-  bool isValidName(String str) {
-    final number = num.tryParse(str);
-
-    if (number == null) {
-      return true;
-    }
-    return false;
+  /// check if the string [str] contains only letters
+  bool isName(String str) {
+    return _alpha.hasMatch(str);
   }
 
-  bool isValidEmail(String str) {
-    if (str.contains("@")) {
-      return true;
-    }
-    return false;
+  /// check if the string [str] is an email
+  bool isEmail(String str) {
+    return _email.hasMatch(str.toLowerCase());
   }
 
-  bool isValidPassword(String str) {
-    if (str.length > 4) {
-      return true;
-    }
-    return false;
+  /// check if the string [str] contains at least 8 characters.
+  /// [str] must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
+  ///
+  /// Can contain special characters
+  bool isPasswordStrong(String str) {
+    return _passwordStrong.hasMatch(str);
+  }
+
+  /// check if the string [str] contains only letters and numbers
+  bool isAlphanumeric(String str) {
+    return _alphanumeric.hasMatch(str);
+  }
+
+  /// check if the string [str] contains only letters
+  bool isAlpha(String str) {
+    return _alpha.hasMatch(str);
+  }
+
+  /// check if the string [str] contains only number
+  bool isNum(String str) {
+    return _numeric.hasMatch(str);
+  }
+
+  /// check if the string [str] and comparison are equal as string
+  bool isEquals(String str, comparison) {
+    return str == comparison.toString();
+  }
+
+  /// check if the string [str] is lowercase
+  bool isLowercase(String str) {
+    return str == str.toLowerCase();
+  }
+
+  /// check if the string [str] is uppercase
+  bool isUppercase(String str) {
+    return str == str.toUpperCase();
   }
 }
--- test/package_test.dart
+++ test/package_test.dart
@@ -5,19 +5,60 @@ import 'package:flutter_ii_package_epitech_zhiwen/package.dart';
 void main() {
   test('Is valid sign in information', () {
     final validators = Validators();
-    expect(validators.isValidEmail('zhiwen.wang@epitech.eu'), true);
-    expect(validators.isValidEmail('zhiwen.wang.epitech.eu'), false);
-    expect(validators.isValidPassword('123456'), true);
-    expect(validators.isValidPassword('bad'), false);
+    expect(validators.isEmail('zhiwen.wang@epitech.eu'), true);
+    expect(validators.isEmail('zhiwen.wang.epitech.eu'), false);
+    expect(validators.isPasswordStrong('Test0123'), true);
+    expect(validators.isPasswordStrong('bad'), false);
   });
 
   test('Is valid sign up information', () {
     final validators = Validators();
-    expect(validators.isValidName('zhiwen'), true);
-    expect(validators.isValidName('1234'), false);
-    expect(validators.isValidEmail('zhiwen.wang@epitech.eu'), true);
-    expect(validators.isValidEmail('zhiwen.wang.epitech.eu'), false);
-    expect(validators.isValidPassword('123456'), true);
-    expect(validators.isValidPassword('bad'), false);
+    expect(validators.isName('zhiwen'), true);
+    expect(validators.isName('1234'), false);
+    expect(validators.isEmail('zhiwen.wang@epitech.eu'), true);
+    expect(validators.isEmail('zhiwen.wang.epitech.eu'), false);
+    expect(validators.isPasswordStrong('Test0123'), true);
+    expect(validators.isPasswordStrong('bad'), false);
+  });
+  test('Is alpha numeric', () {
+    final validators = Validators();
+    expect(validators.isAlphanumeric('zhiwen'), true);
+    expect(validators.isAlphanumeric('zhiwen1'), true);
+    expect(validators.isAlphanumeric('))))(((('), false);
+  });
+
+  test('Is alpha', () {
+    final validators = Validators();
+    expect(validators.isAlpha('zhiwen'), true);
+    expect(validators.isAlpha('012345'), false);
+    expect(validators.isAlpha('zhiwen01'), false);
+  });
+
+  test('Is numeric', () {
+    final validators = Validators();
+    expect(validators.isNum('012345'), true);
+    expect(validators.isNum('zhiwen'), false);
+    expect(validators.isNum('zhiwen01'), false);
+  });
+
+  test('Is equals', () {
+    final validators = Validators();
+    expect(validators.isEquals('1', 1), true);
+    expect(validators.isEquals('zhiwen', 'zhiwen'), true);
+    expect(validators.isEquals('zhiwen', 'zhiwen1'), false);
+  });
+
+  test('Is uppercase', () {
+    final validators = Validators();
+    expect(validators.isUppercase('ZHIWEN'), true);
+    expect(validators.isUppercase('zhiwen'), false);
+    expect(validators.isLowercase('Zhiwen'), false);
+  });
+
+  test('Is lowercase', () {
+    final validators = Validators();
+    expect(validators.isLowercase('zhiwen'), true);
+    expect(validators.isLowercase('ZHIWEN'), false);
+    expect(validators.isLowercase('Zhiwen'), false);
   });
 }
